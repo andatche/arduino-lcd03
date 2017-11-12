bbc48775611b88dadbbdac81dfaa5f2a7411c0f1
Ben Arblaster <ben@brightbox.co.uk>
2017-11-12 12:56:44 +0000

    Fix function declarations for LCD03::LCD03 and LCD03::begin and tidy up some whitespace

--- LCD03.cpp
+++ LCD03.cpp
@@ -25,17 +25,12 @@
 
 char _i2c_address;
 
-LCD03::LCD03(char i2c_address) {
+LCD03::LCD03(char i2c_address=I2C_ADDR) {
   // Convert 8-bit address from LCD to 7-bit address for Wire
   _i2c_address = i2c_address>>1;
 }
 
-LCD03::LCD03(char i2c_address) {
-    // Convert 8-bit address from LCD to 7-bit address for Wire
-    _i2c_address = i2c_address>>1;
-}
-
-void LCD03::begin(uint8_t cols, uint8_t rows, uint8_t sda_pin=SDA, uint8_t scl_pin) {
+void LCD03::begin(uint8_t cols, uint8_t rows, uint8_t sda_pin=SDA, uint8_t scl_pin=SCL) {
   Wire.begin(sda_pin, scl_pin);
   noCursor();
   clear();
